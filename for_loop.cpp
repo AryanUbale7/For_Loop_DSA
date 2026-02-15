9f9b4801c7844fcae4b0d418a67731c7016f600e
Aryan Ubale <aryanubale318@gmail.com>
2026-02-16 00:13:49 +0530

    modified code

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a;
+    cout<<"Enter Your Number: "<<endl;
+    cin>>a;
 
-    for(int i=1;i<=5;i++){
+    for(int i=a;i<=100 ; i++){
         cout<<i<<endl;
     }
+return 0;
 }
